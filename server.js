2303c43d3bff3f7edf7b9d3075ccd2312bb169de
BurhanCabiroglu <35686217+BurhanCabiroglu@users.noreply.github.com>
2021-02-25 01:07:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.listen(process.env.PORT||3000,
 */
 
 
-app.listen(process.env.PORT||3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT, () => console.log("Server is running..."));
 
 
 
